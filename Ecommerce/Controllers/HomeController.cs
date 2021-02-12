0eb92b9bccc500024997a67f387c5cef6bcb35bc
mery-19 <meryam-soussi@hotmail.com>
2021-02-12 17:21:39 +0100

    start home page

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -4,15 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
 
 namespace Ecommerce.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View();
+            List<Produit> produits = db.Produits.ToList();
+            List<ProduitDetails> myProduits = new List<ProduitDetails>();
+
+            foreach (Produit produit in produits)
+            {
+                ProduitDetails produitDetails = new ProduitDetails();
+
+                produitDetails.Produit = produit;
+                produitDetails.real_price = produit.prix_vente + (produit.prix_vente * produit.tva) / 100;
+                if (produit.id_promotion != null)
+                {
+                    Promotion p = db.Promotions.Find(produit.id_promotion);
+                    produitDetails.save_price = (produitDetails.real_price * p.taux_promotion) / 100;
+                    produitDetails.deal_price = produitDetails.real_price - produitDetails.save_price;
+                }
+
+                myProduits.Add(produitDetails);
+            }
+            return View(myProduits.ToPagedList(page ?? 1, 10));
         }
 
         public ActionResult About()
