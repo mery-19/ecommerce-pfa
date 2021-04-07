3b3880a9e2eb7f393852078ed7b6394db2c4a194
mery-19 <meryam-soussi@hotmail.com>
2021-04-07 01:24:14 +0100

    add notifications and commandes

---
+++ Ecommerce/Controllers/ControllersApi/CommandeApiController.cs
@@ -0,0 +1,52 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Ecommerce.Controllers.ControllersApi
+{
+    public class CommandeApiController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpGet]
+        public IEnumerable<Commande> Get(int id, String username)
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+            ApplicationUser user = new ApplicationUser();
+
+            user = db.Users.Where(x => x.UserName.Equals(username)).FirstOrDefault();
+            if (user != null)
+            {
+                var commandes = db.Commandes.Where(x => x.Panier.User.UserName.Equals(user.UserName));
+                    commandes = (id == 2) ? commandes.Where(x => x.id_status == id) : ((id==1)?commandes.Where(x => x.id_status == 1):commandes);
+
+                return commandes.OrderByDescending(x => x.date_ajout);
+            }
+            return null;
+        }
+        [HttpGet]
+        public IEnumerable<LigneProduit> showProductInCommande(int id_commande)
+        {
+            Commande commande = db.Commandes.Find(id_commande);
+            List<LignePanier> lignes = db.LignePaniers.Where(x => x.id_panier == commande.id_panier).ToList();
+            List<LigneProduit> lesProduits = new List<LigneProduit>();
+            foreach (LignePanier l in lignes)
+            {
+                LigneProduit my = new LigneProduit();
+                my.name = l.Produit.name;
+                my.image = l.Produit.image;
+                my.quantite = l.quantite;
+                my.prix = l.prix_total;
+                lesProduits.Add(my);
+            }
+            return lesProduits;
+        }
+    }
+}
---
+++ Ecommerce/Controllers/ControllersApi/NotifictionsApiController.cs
@@ -0,0 +1,60 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ecommerce.Controllers.ControllersApi
+{
+    public class NotifictionsApiController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/NotifictionsApi
+        public IEnumerable<Commande> Get(string id_user)
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+            float notif_num = 0;
+
+            /*PageSize: Number of items*/
+            var commandes = db.Commandes.Where(x => x.Panier.id_user.Equals(id_user) && x.id_status == 2);
+
+            /*--START-- to know the num of notifications and restart not*/
+            UserNotification userNotification = db.UserNotifications.Where(x => x.id_user == id_user).FirstOrDefault();
+            notif_num = userNotification.num;
+            userNotification.num = 0;
+            db.Entry(userNotification).State = EntityState.Modified;
+            db.SaveChanges();
+            /*--END-- to know the num of notifications and restart not*/
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return commandes.OrderByDescending(x => x.id);
+        }
+
+        // GET: api/NotifictionsApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/NotifictionsApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/NotifictionsApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/NotifictionsApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
